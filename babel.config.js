3d6a273c8e9110e035bff6969f25d5be710cf788
Carlos Sosa <cesosag@gmail.com>
2020-05-14 17:37:08 -0400

    Refactor Babel config

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	presets: [
+		'@babel/preset-env',
+		'@babel/preset-react',
+	],
+	plugins: [
+		'babel-plugin-styled-components',
+		'react-hot-loader/babel',
+	],
+}
--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 /* eslint-disable no-console */
-const fs = require('fs')
-
-const getBabelConf = () => {
-	try {
-		return JSON.parse(fs.readFileSync(`${__dirname}/.babelrc`))
-	} catch (err) {
-		console.error(err)
-		return err
-	}
-}
-
-const { presets } = getBabelConf()
-require('@babel/register')({ presets })
+const babelConfig = require('./babel.config')
+require('@babel/register')({ ...babelConfig })
 require('asset-require-hook')({
 	extensions: ['jpg', 'png', 'svg', 'gif'],
 	name: 'assets/[name].[ext]',
